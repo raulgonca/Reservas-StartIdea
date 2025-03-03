7e5f0d5415069af1cc96b6811ddfd6d4bc286686
larry <elchinoyoffre@gmail.com>
2025-03-03 17:15:58 +0100

    feat(dashboard): actualizar encabezado y mejorar descripción en Dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -5,55 +5,15 @@ export default function Dashboard() {
     return (
         <AuthenticatedLayout
             header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat sapiente nam sequi, aspernatur nihil voluptates magnam expedita obcaecati ullam distinctio iure et aliquid repudiandae esse at. Illum, veniam illo! Debitis.
-                    
-                </h2>
+                <h1 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                    En este dashboard podrás agregar métricas, comentarios, blogs y otras funcionalidades necesarias, así como destacar noticias relevantes de los espacios de trabajo.
+                </h1>
+
             }
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <p className="text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat sapiente nam sequi, aspernatur nihil voluptates magnam expedita obcaecati ullam distinctio iure et aliquid repudiandae esse at. Illum, veniam illo! Debitis.</p>
-                        
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <p className="text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat sapiente nam sequi, aspernatur nihil voluptates magnam expedita obcaecati ullam distinctio iure et aliquid repudiandae esse at. Illum, veniam illo! Debitis.</p>
-                        
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <p className="text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat sapiente nam sequi, aspernatur nihil voluptates magnam expedita obcaecati ullam distinctio iure et aliquid repudiandae esse at. Illum, veniam illo! Debitis.</p>
-                        
-                        </div>
-                        <p className="text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat sapiente nam sequi, aspernatur nihil voluptates magnam expedita obcaecati ullam distinctio iure et aliquid repudiandae esse at. Illum, veniam illo! Debitis.</p>
-                    </div>
-                </div>
-            </div>
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <p className="text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat sapiente nam sequi, aspernatur nihil voluptates magnam expedita obcaecati ullam distinctio iure et aliquid repudiandae esse at. Illum, veniam illo! Debitis.</p>
-                        
-                        </div>
-                    </div>
-                </div>
-            </div>
+
         </AuthenticatedLayout>
     );
 }
