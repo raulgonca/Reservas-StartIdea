d96728fe1e6f0ae08875dfffc965491f3cb224d5
larry <elchinoyoffre@gmail.com>
2025-03-06 17:11:47 +0100

    feat(welcome): ajustar el diseño de la cuadrícula de espacios para mejorar la presentación

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -211,7 +211,7 @@ export default function Welcome({
                             </div>
 
                             {/* Grid Responsive de Espacios */}
-                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
+                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 sm:gap-8">
                                 {espacios?.map((espacio) => (
                                     <SpaceCard
                                         key={espacio.id}
